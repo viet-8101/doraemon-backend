5179af0c64df6f6c09b7b515293e39f2f1af9117
viet-8101 <vieth8101@gmail.com>
2025-07-21 16:42:41 +0700

    Update server.js

--- server.js
+++ server.js
@@ -10,9 +10,8 @@ dotenv.config(); // Gọi config sau khi import
 const app = express();
 const PORT = process.env.PORT || 3000; // Sử dụng cổng của Render hoặc 3000 nếu chạy cục bộ
 
-// CORS an toàn, chỉ cho phép frontend truy cập
 app.use(cors({
-    origin: 'https://viet-8101.github.github.io' // Đảm bảo đúng địa chỉ frontend của bạn
+    origin: 'https://viet-8101.github.io/giai-ma-doraemon/' // ĐÃ SỬA LẠI ĐÚNG ĐỊA CHỈ FRONTEND
 }));
 
 app.use(express.json());
